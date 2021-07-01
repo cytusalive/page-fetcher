e332dc2abea5993b30a8d82703ce70d19b701d18
cytusalive <cytusomega@gmail.com>
2021-07-01 05:36:36 +0000

    url

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const fs = require('fs');
-request('http://www.example.edu', (error, response, body) => {
+
+const url = 'https://www.google.com';
+request(url, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   fs.writeFile('./index.html', body, err => {
